4c63cc045f6cdfea07d3a75f1329d377a1c1acc0
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-07-21 15:58:37 -0300

    syms2elf 2.0

--- ida_kernwin.py
+++ ida_kernwin.py
@@ -1,14 +1,6 @@
 # ida_kernwin wrapper (syms2elf)
 # @author lautalom
 # @category layer
-<<<<<<< HEAD
-import ghidra_bridge
-
-def ask_form(*args):
-    with ghidra_bridge.GhidraBridge(namespace=globals()):
-        return askYesNo("Continue?", args[0])
-=======
 
 def ask_form(*args):
     return askYesNo("Continue?", args[0])
->>>>>>> pyhidra
