cffb0516699045c8a8d7b21fb4307fa51425b8d8
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-10-23 19:15:18 -0300

    ida_bytes

--- ida_bytes.py
+++ ida_bytes.py
@@ -1,28 +1,56 @@
 # module for IDA Plugin SDK API wrapper: bytes
-# @category GCL
+# @author lautalom
+# @category layer
 
 
 """module for IDA Plugin SDK API wrapper: bytes"""
 
 from array import array
+import uefi_analyser.cp as cp
+from ghidra.program.database.mem.MemoryBlockDB import getBytes
 from ghidra.program.flatapi import FlatProgramAPI
-from ghidra.program.model.mem import MemoryAccessException
-import cp
-import sys
+from idaapi import BADADDR
 
 
 def get_bytes(start, length):
     """Returns length bytes from start of a segment"""
-    res = bytearray(length+1)
-    fcp = FlatProgramAPI(cp.currentProgram)
+    res = b""
+    start += cp.currentProgram.minAddress.getOffset()
     try:
-        res = fcp.getBytes(fcp.toAddr(start), length + 1)
+        res = FlatProgramAPI(cp.currentProgram).getBytes(
+            FlatProgramAPI(cp.currentProgram).toAddr(start), length + 1)
         res = list(res)
         res = [i if i >= 0 else (256 + i) for i in res]
         res = array("B", res)
-    except MemoryAccessException as ex:
-        print(str(ex))
     except:
-        print("Unexpected error:", sys.exc_info()[0])
-    finally:
-        return res
+        print("Could not get bytes")
+    return bytes(res)
+
+
+def next_head(ea, maxea):
+    """get the next code unit that starts at an address 
+    that is greater than the given address (ea)."""
+    fcp = FlatProgramAPI(cp.currentProgram)
+    listing = cp.currentProgram.getListing()
+    codeUnit = listing.getCodeUnitAfter(
+        fcp.toAddr(cp.currentProgram.minAddress.getOffset()+ea))
+    if codeUnit is None:
+        return int(BADADDR,16)
+    elif codeUnit.getAddress().getOffset() > maxea:
+        #maxea is an absolute address
+        return int(BADADDR, 16)
+    reladdr = int(codeUnit.getAddressString(False, False), 16)
+    return reladdr - cp.currentProgram.minAddress.getOffset()
+
+
+def del_items(ea, flags=0, nbytes=1, may_destroy=None):
+    pass
+
+def create_struct(ea, length, tid, force=False):
+    pass
+
+def get_wide_word(ea):
+    return get_bytes(ea, 2)
+
+def get_wide_dword(ea):
+    return get_bytes(ea, 4)
