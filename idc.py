670be4dfbebdba29611fdcaafa5d94a49f820600
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-10-24 14:03:04 -0300

    idc

--- idc.py
+++ idc.py
@@ -2,10 +2,19 @@
 
 from array import array
 from ghidra.program.flatapi import FlatProgramAPI
+from ghidra.program.model.lang import OperandType
+import ida_bytes
 import cp
+import ida_ua
+import ida_name
 
 SEGATTR_END = 8
 
+get_name = ida_name.get_name
+set_cmt = ida_bytes.set_cmt
+get_wide_word = ida_bytes.get_wide_word
+get_wide_dword = ida_bytes.get_wide_dword
+get_wide_byte = ida_bytes.get_wide_byte
 
 def get_segm_attr(segea, attr):
     """
@@ -64,3 +73,98 @@ def get_func_name(func):
     listing = cp.currentProgram.getListing()
     function = listing.getFunctionAt(fcp.toAddr(minAddress+func))
     return function.getName()
+
+def auto_wait():
+    pass
+
+
+def import_type(idx, type_name):
+    if type_name == "EFI_GUID":
+        return 1
+    elif type_name == "EFI_SYSTEM_TABLE":
+        return 2
+    elif type_name == "EFI_RUNTIME_SERVICES":
+        return 3
+    elif type_name == "EFI_BOOT_SERVICES":
+        return 4
+    else:
+        return 5
+
+
+def get_segm_start(ea):
+    fcp = FlatProgramAPI(cp.currentProgram)
+    minAddress = cp.currentProgram.minAddress.getOffset()
+    block = cp.currentProgram.getMemory().getBlock(
+        fcp.toAddr(ea+minAddress)).getStart().getOffset()
+    return block-minAddress
+
+
+def get_segm_end(ea):
+    fcp = FlatProgramAPI(cp.currentProgram)
+    minAddress = cp.currentProgram.minAddress.getOffset()
+    block = cp.currentProgram.getMemory().getBlock(
+        fcp.toAddr(ea+minAddress)).getEnd().getOffset()
+    return block-minAddress
+
+
+def print_insn_mnem(ea):
+    fcp = FlatProgramAPI(cp.currentProgram)
+    minAddress = cp.currentProgram.minAddress.getOffset()
+    listing = cp.currentProgram.getListing()
+    codeUnit = listing.getCodeUnitAt(fcp.toAddr(minAddress+ea))
+    if codeUnit is not None:
+        return str(codeUnit.getMnemonicString().lower())
+    else:
+        return ""
+
+
+def get_operand_value(ea,n):
+    fcp = FlatProgramAPI(cp.currentProgram)
+    minAddress = cp.currentProgram.minAddress.getOffset()
+    listing = cp.currentProgram.getListing()
+    codeUnit = listing.getCodeUnitAt(fcp.toAddr(minAddress+ea))
+    if codeUnit is None:
+        return -1
+    insn = ida_ua.insn_t()
+    inslen = ida_ua.decode_insn(insn, ea)
+    if inslen == 0 or n>=codeUnit.getNumOperands():
+        return -1
+    op, value = insn.ops[n], -1
+    if not op:
+        return -1
+    
+    if op.type & OperandType.REGISTER:
+        value = op.reg
+    elif op.type & OperandType.SCALAR and not\
+        (op.type & OperandType.ADDRESS):
+        value = op.value
+    if op.type & OperandType.CODE and \
+        op.type & OperandType.ADDRESS:
+        value = -1
+    if op.type & OperandType.ADDRESS and op.type & OperandType.DATA:
+        value = op.addr
+    if op.type & OperandType.ADDRESS and op.type & OperandType.SCALAR:
+        value = op.addr
+    else:
+        value = -1
+
+    return value
+
+def next_head(ea, maxea=cp.currentProgram.maxAddress.getOffset()):
+    return ida_bytes.next_head(ea, maxea) 
+
+
+def get_struc_id(struc):
+    if struc == "EFI_GUID":
+        return 1
+    elif struc == "EFI_BOOT_SERVICES":
+        return 2
+
+def SetType(ea, newtype):
+    pass
+
+def set_name(ea, name, flags=0):
+    pass
+
+def op_stroff(ea, n, strid, delta):
+    pass
