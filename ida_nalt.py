8e8064547ce7e492f3220542cf3985a5fe079136
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-10-23 22:07:06 -0300

    ida_nalt

--- ida_nalt.py
+++ ida_nalt.py
@@ -1,7 +1,10 @@
-# ida_nalt wrapper (syms2elf)
+# ida_nalt wrapper
 # @category GCL
 
 import cp
 
 def get_input_file_path():
     return cp.currentProgram.getExecutablePath()
+
+def get_imagebase():
+    return cp.currentProgram.getImageBase()
