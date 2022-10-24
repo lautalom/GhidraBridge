d9564439fd4fd8a02e31851ef84544161ad49a0b
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-10-23 21:37:59 -0300

    ida_kernwin

---
+++ ida_graph.py
@@ -0,0 +1,29 @@
+
+class GraphViewer:
+    UI_Hooks_Trampoline = None
+    def __init__(self, title="", close_open=False):
+        self.title = title
+        self.close_open = close_open
+        return
+    def AddCommand(self,title, shortcut):
+        return
+    def AddEdge(self, src_node, dest_node):
+        return
+    def AddNode(self, obj):
+        return
+    def Clear(self):
+        return
+    def Close(self):
+        return
+    def Count(self):
+        return
+    def OnCommand(self, cmd_id):
+        return
+    def OnPopup(self, widget, popup_handle):
+        return
+    def OnRefresh(self):
+        return
+    def Select(self, node_id):
+        return
+    def Show(self):
+        return
--- ida_kernwin.py
+++ ida_kernwin.py
@@ -3,3 +3,29 @@
 
 def ask_form(*args):
     pass
+
+def ask_str(*args):
+    defval = ""
+    histId = ""
+    prompt = ""
+    if len(args)>0:
+        defval = args[0]
+    if len(args)>1:
+        histId = args[1]
+    if len(args)>2:
+        prompt = args[2]
+    return 1
+
+class action_handler_t:
+
+    def __init__(self, *args):
+        pass
+
+class action_desc_t:
+
+    def __init__(self, *args):
+        pass
+
+
+def attach_dynamic_action_to_popup(*args):
+    pass
