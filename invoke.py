fd098d9733e40c1df278b13c13bb151ddb21a6d3
Lautaro Lombardi <lautaro.lombardi@mi.unc.edu.ar>
2022-10-27 15:01:31 -0300

    invoke

--- invoke.py
+++ invoke.py
@@ -4,21 +4,18 @@
 if __name__ == '__main__':
     try:
         import importlib.util
-        import cp
-    except:
         import sys
         import os
-        sys.path.append(os.path.join(os.path.dirname(__file__)))
-        # sys.path.append(os.path.join(os.path.dirname(__file__),'uefi_analyser'))
         import cp
-        print(sys.path)
+    except:
+        sys.path.append(os.path.join(os.path.dirname(__file__)))
+        import cp
     finally:
-        plugin = "uefi_analyser"
+        plugin = "findcrypt3"
         cp.currentProgram = currentProgram
         path = os.path.dirname(__file__)+'/'+plugin+'.py'
         spec = importlib.util.spec_from_file_location(plugin,path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
-        # importlib.import_module(plugin)
         module.PLUGIN_ENTRY().run(0)
         print("Done")
