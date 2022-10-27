8e8eb6cafe291c01ff3df91c9abf0f4a5936f6f3
lautaro lombardi <lautaro.lombardi@eclypsium.com>
2022-10-27 14:32:53 -0300

    invoke

--- invoke.py
+++ invoke.py
@@ -1,20 +1,24 @@
-# @author lautalom
 # @category layer
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     try:
         import importlib.util
         import cp
     except:
         import sys
         import os
-        sys.path.append(os.path.dirname(__file__))
-        print(sys.path)
+        sys.path.append(os.path.join(os.path.dirname(__file__)))
+        # sys.path.append(os.path.join(os.path.dirname(__file__),'uefi_analyser'))
         import cp
+        print(sys.path)
     finally:
-        plugin = "analyser" 
+        plugin = "uefi_analyser"
         cp.currentProgram = currentProgram
-        module = importlib.import_module(plugin)
+        path = os.path.dirname(__file__)+'/'+plugin+'.py'
+        spec = importlib.util.spec_from_file_location(plugin,path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        # importlib.import_module(plugin)
         module.PLUGIN_ENTRY().run(0)
         print("Done")
